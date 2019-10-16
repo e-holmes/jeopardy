a4878f6031f4fb82e35cab1fddc17758f80ba35e
e-holmes <bethholmes768@gmail.com>
2019-10-16 15:14:56 -0700

    added in reset button and checking deployed version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,105 +1,110 @@
 $(document).ready(function () {
-    var table; 
+    var table = "<table style='height:200%'><tr><th>Column1</th><th>Column2</th><th>Column3</th><th>Column4</th><th>Column5</th></tr><tr><td><button id='c1r2'>2 Stars!</button></td><td><button id='c2r2'>2 Stars!</button></td><td><button id='c3r2'>2 Stars!</button></td><td><button id='c4r2'>2 Stars!</button></td><td><button id='c5r2'>2 Stars!</button></td></tr><tr><td><button id='c1r3'>3 Stars!</button></td><td><button id='c2r3'>3 Stars!</button></td><td><button id='c3r3'>3 Stars!</button></td><td><button id='c4r3'>3 Stars!</button></td><td><button id='c5r3'>3 Stars!</button></td></tr><tr><td><button id='c1r4'>4 Stars!</button></td><td><button id='c2r4'>4 Stars!</button></td><td><button id='c3r4'>4 Stars!</button></td><td><button id='c4r4'>4 Stars!</button></td><td><button id='c5r4'>4 Stars!</button></td></tr><tr><td><button id='c1r5'>5 Stars!</button></td><td><button id='c2r5'>5 Stars!</button></td><td><button id='c3r5'>5 Stars!</button></td><td><button id='c4r5'>5 Stars!</button></td><td><button id='c5r5'>5 Stars!</button></td></tr><tr><td><button id='c1r6'>6 Stars!</button></td><td><button id='c2r6'>6 Stars!</button></td><td><button id='c3r6'>6 Stars!</button></td><td><button id='c4r6'>6 Stars!</button></td><td><button id='c5r6'>6 Stars!</button></td></tr></table>"; 
     var onTable = "true";
-    var question;
+    var question = "PaceHolder";
+
 
 // FIREBASE
-    var firebaseConfig = {
-        apiKey: "AIzaSyCGqvbJZymau1ztidkgJNAgGQptLqjynls",
-        authDomain: "jeapordy-92285.firebaseapp.com",
-        databaseURL: "https://jeapordy-92285.firebaseio.com",
-        projectId: "jeapordy-92285",
-        storageBucket: "jeapordy-92285.appspot.com",
-        messagingSenderId: "452569277380",
-        appId: "1:452569277380:web:8f8b9d0a09458173a4fa91"
-    };
-    
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+    apiKey: "AIzaSyCGqvbJZymau1ztidkgJNAgGQptLqjynls",
+    authDomain: "jeapordy-92285.firebaseapp.com",
+    databaseURL: "https://jeapordy-92285.firebaseio.com",
+    projectId: "jeapordy-92285",
+    storageBucket: "jeapordy-92285.appspot.com",
+    messagingSenderId: "452569277380",
+    appId: "1:452569277380:web:8f8b9d0a09458173a4fa91"
+};
 
-    // Firesbase Pull && Page Update
-    firebase.database().ref().on("value", function(snapshot) {
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+// Firesbase Pull && Page Update
+firebase.database().ref().on("value", function(snapshot) {
+    $("#reset").on("click", function(){
+        table = "<table style='height:200%'><tr><th>Column1</th><th>Column2</th><th>Column3</th><th>Column4</th><th>Column5</th></tr><tr><td><button id='c1r2'>2 Stars!</button></td><td><button id='c2r2'>2 Stars!</button></td><td><button id='c3r2'>2 Stars!</button></td><td><button id='c4r2'>2 Stars!</button></td><td><button id='c5r2'>2 Stars!</button></td></tr><tr><td><button id='c1r3'>3 Stars!</button></td><td><button id='c2r3'>3 Stars!</button></td><td><button id='c3r3'>3 Stars!</button></td><td><button id='c4r3'>3 Stars!</button></td><td><button id='c5r3'>3 Stars!</button></td></tr><tr><td><button id='c1r4'>4 Stars!</button></td><td><button id='c2r4'>4 Stars!</button></td><td><button id='c3r4'>4 Stars!</button></td><td><button id='c4r4'>4 Stars!</button></td><td><button id='c5r4'>4 Stars!</button></td></tr><tr><td><button id='c1r5'>5 Stars!</button></td><td><button id='c2r5'>5 Stars!</button></td><td><button id='c3r5'>5 Stars!</button></td><td><button id='c4r5'>5 Stars!</button></td><td><button id='c5r5'>5 Stars!</button></td></tr><tr><td><button id='c1r6'>6 Stars!</button></td><td><button id='c2r6'>6 Stars!</button></td><td><button id='c3r6'>6 Stars!</button></td><td><button id='c4r6'>6 Stars!</button></td><td><button id='c5r6'>6 Stars!</button></td></tr></table>"; 
+        onTable = "true";
+        question = "PaceHolder";
+        firebasePageStatus(table, onTable, question);
+        location.reload();
+    })
+    // log contents
     contents = snapshot.val();
     console.log(contents);
     if (contents == null){
+        // store initial versions
+        firebasePageStatus(table, onTable, question);
+    }  
+    // fetch table status 
+    onTable = snapshot.val().data.pageStatus;
+    if (onTable == "true"){ //TABLE SHOULD SHOW
+        // fetch table
+        tableStored = snapshot.val().data.tableStore;
+        
+        // empty table and question display
         $(".table-load").empty();
         $("#Qdisplay").empty();
-        inGameBoard();
+        
+        // place table
+        $(".table-load").append(tableStored);
+        
+        // set table triggers
         GameBoardLoad();
-        tableUpdate();
-        questionUpdate();
-        firebasePageStatus(table, onTable, question);
-    } else {   
-        console.log("Firebase not empty"); 
-        onTable = snapshot.val().onTable.pageStatus;
-        console.log("Checking table before set" +onTable);
 
-        if (onTable == "true"){
-            tableStored = snapshot.val().table.tableStore;
-            console.log ("TABLE STORED TO BE APPENDED");
-            $(".table-load").empty();
-            $("#Qdisplay").empty();
-            $(".table-load").append(tableStored);
-            console.log("Appended table");
-            GameBoardLoad();
-            console.log("TABLE HAS BEEN PUSH HALT TILL CLICK");
-        } else if (onTable == "false") {     
-            question = snapshot.val().quest.questStore;
-            console.log("question stored for appending");
-            $(".table-load").empty();
-            $("#Qdisplay").empty();
-            $("#Qdisplay").append(question);
-            $("#Qdisplay").append("<button id='submit'>Return</button>");
-            console.log("Appended question");
-            $("#submit").on("click", function() {
-                console.log("submit clicked");
-                onTable = "true";
-                firebasePageStatus(table, onTable, question);
-                location.reload();
-            })
+        console.log("TABLE HAS BEEN PUSH HALT TILL CLICK");
+    } else if (onTable == "false") {   //QUESTION SHOWS
+        // fetch question  
+        question = snapshot.val().data.questStore;
+ 
+        // empty table and question display
+        $(".table-load").empty();
+        $("#Qdisplay").empty();
+
+        // place question && Submit Button
+        $("#Qdisplay").append(question +"<button id='submit'>Return</button>");
+  
+        // Set button click response
+        $("#submit").on("click", function() {
+            console.log("submit clicked");
+            // Change page to table
+            onTable = "true";
+            // store table, page, and question
+            firebasePageStatus(table, onTable, question);
+            // reload page
+            location.reload();
+        })
         }
-    }
-
     });
 
-// FUNCTIONS
+    
+
+    // FUNCTIONS
     function questionUpdate(qid){
         question = $("."+qid).html();
         console.log("QUESTION UPDATE STORAGE");
-    }
+    };
 
     function tableUpdate(){
         table = $(".table-load").html();
         console.log("TABLE UPDATE STORAGE");
         $("#Qdisplay").empty();
         
-    }
+    };
     
     function firebasePageStatus(data1, data2, data3){
-        var tableRef = firebase.database().ref("table/");
-        tableRef.set ({ tableStore: data1});
-        console.log("Stored table: ");
+        var dataRef = firebase.database().ref("data/");
+        dataRef.set ({ 
+            tableStore: data1,
+            pageStatus: data2,
+            questStore: data3
+        });
+        console.log("Stored table, page status, question");
 
-        console.log("PAGESTATUS UPDATE: " +data2);
-        var onTableRef = firebase.database().ref("onTable/");
-        onTableRef.set ({ pageStatus: data2}); 
-        console.log("Stored page status: " );
-            
-        var questRef = firebase.database().ref("quest/");
-        questRef.set ({ questStore: data3 });
-        console.log("Stored question: ");  
-    }
-
-
-    function inGameBoard(){
-
-        $(".table-load").append(
-            "<table style='height:200%'><tr><th>Column1</th><th>Column2</th><th>Column3</th><th>Column4</th><th>Column5</th></tr><tr><td><button id='c1r2'>2 Stars!</button></td><td><button id='c2r2'>2 Stars!</button></td><td><button id='c3r2'>2 Stars!</button></td><td><button id='c4r2'>2 Stars!</button></td><td><button id='c5r2'>2 Stars!</button></td></tr><tr><td><button id='c1r3'>3 Stars!</button></td><td><button id='c2r3'>3 Stars!</button></td><td><button id='c3r3'>3 Stars!</button></td><td><button id='c4r3'>3 Stars!</button></td><td><button id='c5r3'>3 Stars!</button></td></tr><tr><td><button id='c1r4'>4 Stars!</button></td><td><button id='c2r4'>4 Stars!</button></td><td><button id='c3r4'>4 Stars!</button></td><td><button id='c4r4'>4 Stars!</button></td><td><button id='c5r4'>4 Stars!</button></td></tr><tr><td><button id='c1r5'>5 Stars!</button></td><td><button id='c2r5'>5 Stars!</button></td><td><button id='c3r5'>5 Stars!</button></td><td><button id='c4r5'>5 Stars!</button></td><td><button id='c5r5'>5 Stars!</button></td></tr><tr><td><button id='c1r6'>6 Stars!</button></td><td><button id='c2r6'>6 Stars!</button></td><td><button id='c3r6'>6 Stars!</button></td><td><button id='c4r6'>6 Stars!</button></td><td><button id='c5r6'>6 Stars!</button></td></tr></table>"
-        );
-        console.log("Set initial board");
-    }
+    };
     
     function GameBoardLoad(){
+
+
         // Button Clicks
         $("#c1r2").on("click", function(){
             onTable = "false";          
@@ -315,7 +320,5 @@ $(document).ready(function () {
         })
 
     }
-
-
 
 });
